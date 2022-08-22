a2300f9bbb4371602d8558bda9e15e8af6ba6d3e
Jeremy5604 <91640498+Jeremy5604@users.noreply.github.com>
2022-08-22 01:32:58 -0500

    Update Leasy.py
    Agregué comentarios

--- Leasy.py
+++ Leasy.py
@@ -1,27 +1,47 @@
 from datetime import datetime
-from flask import Flask
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit
-import wikipedia
+import speech_recognition as sr #Clase para escuchar comandos de voz
+import pyttsx3  #Clase para que Leasy responda con palabras verbales
+import pywhatkit    #Clase para abrir y reproducir videos de youtube
+import wikipedia    #Clase para buscar información en wikipedia
 
 name = "alexa"
-# Ese nombre es provisional, para pruebas , su verdadero nombre es LIZI
-tiempo = datetime.now()
-sample_rate = 48000
-chunk_size = 32
-listener = sr.Recognizer()
-engine = pyttsx3.init()
+# Ese nombre es provisional, para pruebas , su verdadero nombre es Leasy
+tiempo = datetime.now() #tiempo toma el valor de un objeto datetime(hora,min,segundo en tiempo real)
+sample_rate = 48000 # variable que sirve para controlar la tasa de muestra de audio
+chunk_size = 32 #No me acuerdo :v
 
-voices: object = engine.getProperty('voices')
-engine.setProperty('rate', 150)
-engine.setProperty('volume', 10.0)
-engine.setProperty('voice', voices[0].id)
+listener = sr.Recognizer() #listener toma el valor del objeto reconocedor de voz---
+#-- este objeto contiene métodos importantes para el procesamiento de ordenes
+
+engine = pyttsx3.init() # la variable engine toma el valor de objeto pyttsx3, el cual---
+#--- es un objeto que reproduce sonidos, se usará como respuesta cuando Leasy necesite datos más---
+#--- especificos o cuando no pueda procesar una orden
+
+
+voices: object = engine.getProperty('voices') #Son las voces que puede utilizar Leasy, creo que---
+#---existen 4 voces.
+
+
+#A continuación usaré el método(setProperty) "poner propiedades", primero se indica la propiedad---
+#--- y luego el valor a asignarle según el que corresponda
+
+engine.setProperty('rate', 150) #Propiedad Velocidad = 150
+engine.setProperty('volume', 10.0)#Propiedad Volummen = 10.0
+engine.setProperty('voice', voices[0].id) #Propiedad Voz = Elena creo que se llamaba la morra---
+#---Para ver las voces coloque un print dentro de un for e impriman los valores del arreglo voices[].id---
+#--- la palabra "id" significa que estas llamando al nombre de la voz
+
+
 
 
 # 1. Que se active con la palabra "LIZY "(microfono encendido)
+#NOTA: Aún no esta programada para escuchar, luego ejecutar y poner en modo espera hasta que se le vuelva---
+#---a llamar por su nombre, sino que el código a continuación corre, escucha, ejecuta y se queda ---
+#---ejecutanto en segundo plano pero sin escuchar. HAY QUE ARREGLAR ESO 
+
+#Aqui está el método principal, el cual es el único obligatoriamente(revisa línea final)
 def iniciar():
-    while True:
+    while True:# El while es para que nunca deje de ejecutarse una ves se encienda
         with sr.Microphone(sample_rate=sample_rate,
                            chunk_size=chunk_size) as source:
             talk("Escuchando...")
